2b757de0dea32f5e341cd746a606701c451b7dcb
genderegnan <andereggenalexis@gmail.com>
2024-05-27 13:12:18 -0300

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,19 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "ecs" {
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "ecs" {
-  vpc_id     = aws_vpc.ecs.id
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
   cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_security_group" "ecs" {
-  vpc_id      = aws_vpc.ecs.id
-  name        = "ecs-sg"
+resource "aws_security_group" "example" {
+  vpc_id      = aws_vpc.example.id
+  name        = "example-sg"
   description = "Allow HTTP traffic"
+
+  
 
   ingress {
     from_port   = 3000
@@ -31,12 +33,12 @@ resource "aws_security_group" "ecs" {
   }
 }
 
-resource "aws_ecs_cluster" "ecs" {
-  name = "ecs-cluster"
+resource "aws_ecs_cluster" "example" {
+  name = "example-cluster"
 }
 
-resource "aws_ecs_task_definition" "ecs" {
-  family                   = "ecs-task"
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -46,7 +48,7 @@ resource "aws_ecs_task_definition" "ecs" {
 
   container_definitions = jsonencode([
     {
-      name      = "ecs-app"
+      name      = "example-app"
       image     = var.image_url
       essential = true
       portMappings = [
@@ -65,15 +67,15 @@ resource "aws_ecs_task_definition" "ecs" {
   ])
 }
 
-resource "aws_ecs_service" "ecs" {
-  name            = "ecs-service"
-  cluster         = aws_ecs_cluster.ecs.id
-  task_definition = aws_ecs_task_definition.ecs.arn
+resource "aws_ecs_service" "example" {
+  name            = "example-service"
+  cluster         = aws_ecs_cluster.example.id
+  task_definition = aws_ecs_task_definition.example.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
-    subnets = [aws_subnet.ecs.id]
-    security_groups = [aws_security_group.ecs.id]
+    subnets = [aws_subnet.example.id]
+    security_groups = [aws_security_group.example.id]
   }
 }
 
@@ -97,86 +99,4 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# Autoscaling setup
-
-resource "aws_appautoscaling_target" "ecs" {
-  max_capacity       = 10
-  min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.ecs.name}/${aws_ecs_service.ecs.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
-
-resource "aws_appautoscaling_policy" "ecs_scale_up" {
-  name               = "scale-up"
-  resource_id        = aws_appautoscaling_target.ecs.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
-
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = 60
-    metric_aggregation_type = "Average"
-
-    step_adjustment {
-      scaling_adjustment = 1
-      metric_interval_lower_bound = 0
-    }
-  }
-}
-
-resource "aws_appautoscaling_policy" "ecs_scale_down" {
-  name               = "scale-down"
-  resource_id        = aws_appautoscaling_target.ecs.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
-
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = 60
-    metric_aggregation_type = "Average"
-
-    step_adjustment {
-      scaling_adjustment = -1
-      metric_interval_upper_bound = 0
-    }
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
-  alarm_name          = "cpu-utilization-high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "75"
-
-  dimensions = {
-    ClusterName  = aws_ecs_cluster.ecs.name
-    ServiceName  = aws_ecs_service.ecs.name
-  }
-
-  alarm_actions = [aws_appautoscaling_policy.ecs_scale_up.arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "cpu_utilization_low" {
-  alarm_name          = "cpu-utilization-low"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "25"
-
-  dimensions = {
-    ClusterName  = aws_ecs_cluster.ecs.name
-    ServiceName  = aws_ecs_service.ecs.name
-  }
-
-  alarm_actions = [aws_appautoscaling_policy.ecs_scale_down.arn]
 }
