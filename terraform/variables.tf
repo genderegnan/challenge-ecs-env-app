5427e1225576629c50bdc3a10ca40a2194bc7e0b
genderegnan <andereggenalexis@gmail.com>
2024-05-27 17:24:08 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,7 +23,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "bradfordhamilton/crystal_blockchain:latest"
+  default     = "618755546955.dkr.ecr.us-east-1.amazonaws.com/challenge-ecs-env-app:latest"
 }
 
 variable "app_port" {
